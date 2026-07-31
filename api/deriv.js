e1efbb2752a20846810c6e26358c081029d09c80
georgeiphone704-cyber <georgeiphone704@gmail.com>
2026-07-31 21:29:13 +0300

    Update deriv.js
    Update Deriv API connection

--- api/deriv.js
+++ api/deriv.js
@@ -4,55 +4,40 @@ const SYMBOL = "R_100";
 let socket = null;
 
 function connectDeriv() {
-
     socket = new WebSocket(`wss://ws.derivws.com/websockets/v3?app_id=${APP_ID}`);
 
     socket.onopen = () => {
-
         console.log("Connected to Deriv");
 
         socket.send(JSON.stringify({
-            ticks: SYMBOL
+            ticks: SYMBOL,
+            subscribe: 1
         }));
-
     };
 
     socket.onmessage = (event) => {
-
         const data = JSON.parse(event.data);
 
-        if (data.tick) {
-
+        if (data.tick && window.engine) {
             const tick = {
                 quote: data.tick.quote,
                 epoch: data.tick.epoch,
                 digit: Number(String(data.tick.quote).slice(-1))
             };
 
-            console.log(tick);
-
-            if (window.engine) {
-                window.engine.addTick(tick);
-            }
-
+            window.engine.receiveTick(tick);
         }
-
     };
 
     socket.onclose = () => {
-
         console.log("Disconnected... reconnecting");
 
         setTimeout(connectDeriv, 3000);
-
     };
 
     socket.onerror = (error) => {
-
-        console.log(error);
-
+        console.error(error);
     };
-
 }
 
 connectDeriv();
